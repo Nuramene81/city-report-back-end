befb236ac292ffc1644a28737af2bd3308a92800
Nuramene81 <116958206+Nuramene81@users.noreply.github.com>
2024-01-04 14:51:08 -0800

    adjust CORS

--- app.ts
+++ app.ts
@@ -1,9 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 import { Pool } from './db-pool';
 import { userRoutes } from './src/routes/user/userRoutes';
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+  origin: 'http://localhost:4200' // restrict calls to those this address
+}));
 
 const pool = new Pool();
 pool.testPrimaryDBConection();
