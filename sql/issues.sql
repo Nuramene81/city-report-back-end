53b0e5b3873cbe2fe071643b5235b6bb1c213586
Nuramene81 <116958206+Nuramene81@users.noreply.github.com>
2024-01-24 14:57:15 +0200

    update SQL

--- sql/issues.sql
+++ sql/issues.sql
@@ -3,8 +3,8 @@ CREATE TABLE "Issues" (
   "Title" VARCHAR(50),
   "ReportedByUserUUID" UUID NOT NULL,
   "Description" TEXT,
-  "IssueLatitude" DECIMAL(9,15),
-  "IssueLongitude" DECIMAL(9,15),
+  "IssueLatitude" TEXT,
+  "IssueLongitude" TEXT,
   "DateReported" TIMESTAMP,
   "Status" VARCHAR(36),
   FOREIGN KEY ("ReportedByUserUUID") REFERENCES "Users" ("ID")
