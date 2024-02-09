8afaf91d10c31a95e40385f67d199523a210a2c2
Nuramene81 <116958206+Nuramene81@users.noreply.github.com>
2024-02-09 21:18:29 +0200

    change built config options

--- dist/constants.js
+++ dist/constants.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.DB_CONFIG_OPTIONS = void 0;
 exports.DB_CONFIG_OPTIONS = {
-    host: 'localhost',
-    port: 5433,
-    database: 'city-report',
-    user: 'postgres',
-    password: '1504'
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+    database: process.env.DB_DATABASE_NAME || 'postgres',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
 };
