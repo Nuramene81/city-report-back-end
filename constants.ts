2acf4b1825afe631dc3991ab73abc9b6117bc3a7
Nuramene81 <116958206+Nuramene81@users.noreply.github.com>
2024-02-09 17:43:04 +0200

    Add SSL configuration to DB_CONFIG_OPTIONS

--- constants.ts
+++ constants.ts
@@ -3,5 +3,8 @@ export const DB_CONFIG_OPTIONS = {
   port: process.env.DB_PORT,
   database: process.env.DB_DATABASE_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  ssl: {
+    rejectUnauthorized: false
+  }
 }
