14b741205592dfe8d013ed836116031b08d28b9b
Nuramene81 <116958206+Nuramene81@users.noreply.github.com>
2024-02-10 11:48:47 +0200

    Update session cookie settings

--- app.ts
+++ app.ts
@@ -37,8 +37,8 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: false,
-    secure: true,
-    sameSite: 'none',
+    secure: false,
+    sameSite: 'lax',
     maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }));
