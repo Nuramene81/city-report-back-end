86bf2ad08abf0dc2df6d6eb551ba62d15475334b
Nuramene81 <116958206+Nuramene81@users.noreply.github.com>
2024-02-11 12:56:27 +0200

    Commented out unused code and removed user information from issue request

--- helper-functions.ts
+++ helper-functions.ts
@@ -1,10 +1,10 @@
-import { NextFunction, Request, Response } from 'express';
+// import { NextFunction, Request, Response } from 'express';
 
-export function isUserLoggedIn(req: Request, res: Response, next: NextFunction) {
-  if(!req.session.userUUID){
-    res.json({ message: 'You are not logged in' });
-    return;
-  } else {
-    next()
-  }
-}
+// export function isUserLoggedIn(req: Request, res: Response, next: NextFunction) {
+//   if(!req.session.userUUID){
+//     res.json({ message: 'You are not logged in' });
+//     return;
+//   } else {
+//     next()
+//   }
+// }
